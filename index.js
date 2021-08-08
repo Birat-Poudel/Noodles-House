71024a2f04d50ee5a6b7d333a9675ce712f9f882
Birat Poudel <poudel.birat25@gmail.com>
2021-08-08 16:17:46 +0545

    Card component development and code refactoring

--- index.js
+++ index.js
@@ -20,4 +20,55 @@ const navSlide = () => {
     })
 }
 
+const addToCart = () => {
+    const buttons = document.querySelectorAll(".card__button");
+
+    buttons[0].addEventListener('click', () => {
+        buttons[0].classList.toggle("card__button--active");
+        if (buttons[0].classList[1]) {
+            buttons[0].innerHTML = "Added"
+        }
+        else {
+            buttons[0].innerHTML = "Add +"
+        }
+    })
+
+    buttons[1].addEventListener('click', () => {
+        buttons[1].classList.toggle("card__button--active");
+        if (buttons[1].classList[1]) {
+            buttons[1].innerHTML = "Added"
+        }
+        else {
+            buttons[1].innerHTML = "Add +"
+        }
+    })
+
+    buttons[2].addEventListener('click', () => {
+        buttons[2].classList.toggle("card__button--active");
+        if (buttons[2].classList[1]) {
+            buttons[2].innerHTML = "Added"
+        }
+        else {
+            buttons[2].innerHTML = "Add +"
+        }
+    })
+};
+
+const disText = () => {
+    const disappearingText = document.getElementsByClassName("disText");
+    const text = ["Mukbang challenge?", "Unexpected guests?", "Movie night?", "Game night?", "Late night at office?", "Movie marathon?", "Cooking gone wrong?", "Party night?", "Unexpected friends?"]
+
+    var count = 0;
+    setInterval(e => {
+        var randomNumber = (Math.floor(Math.random() + 101));
+        count = (count + randomNumber) % text.length;
+        var newText = text[count];
+        disappearingText[0].innerHTML = newText;
+    }, 2000);
+}
+
 navSlide();
+addToCart();
+disText();
+
+
