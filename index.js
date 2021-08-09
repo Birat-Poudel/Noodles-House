a5b6429a9190630f7e6e26648fbbe2adc07dbc61
Birat Poudel <poudel.birat25@gmail.com>
2021-08-09 14:55:31 +0545

    code refactoring navbar mobile

--- index.js
+++ index.js
@@ -3,6 +3,27 @@ window.addEventListener('scroll', () => {
     header.classList.toggle("sticky", window.scrollY > 0);
 })
 
+const hamBurger = document.querySelector(".hamburger");
+const nav = document.querySelector(".nav__links");
+const navLinks = document.querySelectorAll(".nav__links li");
+
+navLinks.forEach((link, index) => {
+    link.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+        hamBurger.classList.toggle('toggle');
+        nav.classList.toggle('display-off');
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = "";
+            }
+            else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.1}s`;
+            }
+        })
+        }
+    )
+})
+
 const navSlide = () => {
 
     const hamBurger = document.querySelector(".hamburger");
@@ -75,5 +96,3 @@ const disText = () => {
 navSlide();
 addToCart();
 disText();
-
-
